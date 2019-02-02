be91ff7a2bf29b8ab25939c75f5b14958c1f3086
Igor Behen <behen.igor@gmail.com>
2019-02-02 03:07:50 +0200

    Commit 1

---
+++ app/src/main/java/com/pllug/course/behen/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pllug.course.behen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+int d = 9;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
